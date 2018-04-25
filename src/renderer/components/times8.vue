2bb1bad656226ea2703979f11f9b492d62d9f48f
Collider LI <lhc199652@gmail.com>
2018-04-25 23:52:31 +0800

    init commit

---
+++ src/renderer/components/times8.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="container" class="wrapper">
+    <div v-for="timeEight in eights" class="box-8">
+      <div class="number">
+        {{ timeEight }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      eights: []
+    }
+  },
+  created: function () {
+    for (let a = 8; a <= 2208; a = a + 8) {
+      this.eights.push(a)
+    }
+  }
+}
+</script>
+
+<style>
+
+  .box-8 {
+    width: calc((100% - 24px) / 12);
+    height: 48px;
+    background: #111111;
+    border: 1px solid rgba(45,45,45,0.15);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.06s ease-in-out;
+    padding: 12px;
+  }
+
+  .box-8:hover{
+      background:rgba(45,45,45,0.5);
+      border: 1px solid rgba(100,199,255,0.4);
+      transform: scale(1.5);
+  }
+
+  .wrapper {
+    background: #101010;
+    display: flex;
+    flex-wrap: wrap;
+    padding: 24px;
+  }
+
+  .number {
+    font-size:1rem;
+    line-height:1.5rem;
+    color: #666666;
+    text-align: center;
+  }
+
+  .box-8:hover .number{
+      color:#6699ff;
+      font-weight: 700;
+  }
+</style>
---
+++ src/renderer/components/xiaomao.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+    <div v-for="file in files" class="file">
+      <h4>{{ file.name }}</h4>
+      <p>{{ file.size }}k</p>
+      <p v-if="file.size > 20">This file is larger than 20k</p>
+    </div>
+    <div>
+      <h4>New file</h4>
+      <input v-model="newFileName" type="text" placeholder="file name">
+      <br>
+      <input v-model="newFileSize" type="number" placeholder="file size">
+      <br>
+      <button v-on:click="createFile">Create</button>
+    </div>
+  </div>
+</template>
+<!--template: 这个vue object的内容-->
+
+<script>
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      newFileName: '醒醒你生不出小猫的',
+      newFileSize: 233,
+      files: [{name: 'file1.txt', size: 10}, {name: 'file2.txt', size: 20}, {name: 'file3.txt', size: 30}]
+    }
+  },
+  methods: {
+    createFile: function () {
+      this.files.push({
+        name: this.newFileName,
+        size: this.newFileSize
+      })
+    }
+  }
+}
+</script>
+<!-- script:  这个vue object的逻辑-->
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #35495E;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+</style>
+<!-- 这个vue object的样式-->
